d24b16198b7ea93ca55f5881623e986a6b5bdb08
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-06-02 02:54:08 +0900

    gitIgnore 설정

--- src/main/java/com/project/introduceourtown/controller/FlaskController.java
+++ src/main/java/com/project/introduceourtown/controller/FlaskController.java
@@ -1,2 +1,12 @@
-package com.project.introduceourtown.controller;public class FlaskController {
+package com.project.introduceourtown.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequiredArgsConstructor
+public class FlaskController {
+
+    RestTemplate restTemplate = new RestTemplate();
 }
